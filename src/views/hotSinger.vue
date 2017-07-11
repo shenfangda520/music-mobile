2ed74b60ccd68645c70fd6f6d683163304e6c776
shenfangda520 <964003502@qq.com>
2017-07-11 15:21:41 +0800

    网易云第一版

---
+++ src/views/hotSinger.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>热门歌手-尚未开发</div>
+      <div>
+        <img src="../../static/zhangyu01.gif" alt="">
+        <h4>不要着急以后会有的！</h4>
+      </div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+</script>
---
+++ src/views/leaderBoard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>排行榜-尚未开发</div>
+      <div>
+        <img src="../../static/zhangyu2.gif" alt="">
+        <h4>很不好意思哦！</h4>
+      </div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+</script>
